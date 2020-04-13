c2332b428ec1b11b2d69abf9ad1bb6adf48e6ae3
fausio luis <fausioluis@live.com>
2020-04-13 09:28:21 +0200

    Add project files.

---
+++ Collection Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace Collection_Classes
+{
+    class Program
+    {
+        /*
+         
+            Collection is an alternate way of storing multiple variable number of elements or object.
+            Like Stack, Queue, ArrayList, etc all of this are collection classes present in SYSTEM.COLLECTION Namespace.
+            There is no type safety in collection classes i.e in a collection object of a queue we can store few integers and few strings.
+            We need to type cast the elements while accessing it.
+         */
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(3);
+            stack.Push(23);
+            stack.Push(45);
+            stack.Push(78);
+
+            Console.WriteLine(Convert.ToString(stack.Pop()));
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
